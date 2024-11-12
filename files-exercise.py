e8da6539fd40cb650427f5711c8f4a3231a098cc
st34dy34 <hruskadave01@gmail.com>
2024-11-12 19:05:01 +0100

    + Function: rewrite
    Function: rewrite lines from one file to other file

--- files-exercise.py
+++ files-exercise.py
@@ -7,16 +7,20 @@ def Read_file(path):
 def Write_seven_long(text,path):
     with open(path, "w") as f:
         for word in text:
-            print(len(word),word)
             if len(word) > 7:
                 f.write(word)
 
+def Repost(source_path,path):
+    with open(source_path, "r") as x:
+         with open(path, "w") as y:
+            y.writelines(x)
+   
 
 FILE_PATH = "files/source.txt"
 
 text = Read_file(FILE_PATH)
 Write_seven_long(text,"files/sevenplus.txt")
-
+Repost(FILE_PATH, "files/rewrite.txt")
 print(text)
 
 
