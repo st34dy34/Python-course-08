1de71f9acdcc3c0c90b63a218e292d75b1664a82
st34dy34 <hruskadave01@gmail.com>
2024-11-12 19:30:00 +0100

    Almost working Add_asteriks
    Does work if no comma, doesnt work with comma

--- files-exercise.py
+++ files-exercise.py
@@ -4,23 +4,53 @@ def Read_file(path):
         clean_lines = [line.strip() for line in lines]
         return clean_lines
 
+def Write_file(path,text): 
+    with open(path,"w") as f:
+        f.write(text)
+
+def Append_file(path,text): 
+    print(text)
+    with open(path,"a") as f:
+        f.write(text)
+
+
 def Write_seven_long(text,path):
     with open(path, "w") as f:
         for word in text:
             if len(word) > 7:
                 f.write(word)
 
-def Repost(source_path,path):
+def Repost(source_path,output_path):
     with open(source_path, "r") as x:
-         with open(path, "w") as y:
+         with open(output_path, "w") as y:
             y.writelines(x)
    
+def Add_asteriks(path):
+    asteriks = "\n************"
+    text = Read_file(path)
+    happened = False
+    index = 1
+    for line in text:
+        if "," in line:
+            happened = True
+            happend_i = index
+        index += 1
+    if happened == False:
+        Append_file(path, asteriks)
+
+
+
+        
+                
+
+
 
 FILE_PATH = "files/source.txt"
 
 text = Read_file(FILE_PATH)
 Write_seven_long(text,"files/sevenplus.txt")
 Repost(FILE_PATH, "files/rewrite.txt")
+Add_asteriks("files/asteriks.txt")
 print(text)
 
 
